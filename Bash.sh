f5ec45f93f2ca30ebb0a51192f7b6b469ad59597
auraecosystem <sandraisom@icloud.com>
2026-06-16 11:43:55 +0100

    Create Bash.sh

---
+++ Bash.sh
@@ -0,0 +1,21 @@
+python3 build_model_maker_api_docs.mk --output_dir=./docs/api
+# Install mdbook (example)
+curl --proto '=https' --tlsv1.2 https://sh.rustup.rs -sSf -y | sh
+rustup update
+cargo install mdbook
+mdbook build
+# The built site will be in ./book
+docker run --rm -it -v "$(pwd)":/work -w /work makeuniversal-python bash
+# or for node:
+docker run --rm -it -v "$(pwd)":/work -w /work makeuniversal-node bash
+sudo apt update
+sudo apt install -y build-essential gcc g++ make docker.io curl git
+gcc -std=c11 -Wall -Wextra -O2 -o server/server server/server.c
+./server/server
+# Inspect
+less Lmlm.dev/install.sh
+
+# If satisfied, run in a disposable container
+docker run --rm -it -v "$(pwd)":/work -w /work ubuntu:24.04 bash
+apt-get update && apt-get install -y curl python3
+bash Lmlm.dev/install.sh
