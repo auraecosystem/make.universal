d106e25a9d97f4b9359d7e5533db4f901540c8fa
Seriki Walter Yakub <251400999+auraecosystem@users.noreply.github.com>
2026-09-11 16:34:58 +0100

    Create Readme.mk
    Signed-off-by: Seriki Walter Yakub <251400999+auraecosystem@users.noreply.github.com>

---
+++ templates/Readme.mk
@@ -0,0 +1,20 @@
+templates/
+├── c/
+│   ├── main.c.tpl
+│   ├── Makefile.tpl
+│   └── build.mu.tpl
+├── cpp/
+│   ├── Main.cpp.tpl
+│   ├── CMakeLists.txt.tpl
+│   ├── Conan.tpl
+│   └── build.mu.tpl
+├── rust/
+│   └── main.rs.tpl
+├── go/
+│   └── main.go.tpl
+├── python/
+│   └── main.py.tpl
+├── node/
+│   └── index.js.tpl
+└── swift/
+    └── main.swift.tpl
